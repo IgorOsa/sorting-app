9a37587d9829e8f32bf170456beff6e1699c466d
Ihor Osadchyi <igorosa.com@gmail.com>
2021-12-08 18:39:31 +0200

    feat: simple map module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./map/map.module').then((m) => m.MapModule),
+  },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/map/components/map/map.component.ts
@@ -0,0 +1,61 @@
+import {
+  AfterViewInit,
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent implements OnInit, OnChanges, AfterViewInit {
+  public map!: mapboxgl.Map;
+  public ecoStations!: any;
+  public center!: any;
+  public zoom!: any;
+  public style!: string | undefined;
+
+  constructor(private dataService: DataService) {
+    this.ecoStations = this.dataService.store$.value.ecoStations;
+    this.center = this.dataService.store$.value.mapDefaults.center;
+    this.zoom = this.dataService.store$.value.mapDefaults.zoom;
+    this.style = this.dataService.store$.value.mapDefaults.style;
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.dataService.store$.subscribe((store) => {
+      this.ecoStations = store.ecoStations;
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  mapLoadEvent(map: mapboxgl.Map) {
+    console.log('map loaded');
+    this.map = map;
+
+    for (let es of this.ecoStations) {
+      new mapboxgl.Marker({
+        color: '#00FF00',
+      })
+        .setLngLat(es.geo)
+        .addTo(map);
+    }
+  }
+
+  mapClickEvent(event: any) {
+    console.log(event);
+  }
+}
