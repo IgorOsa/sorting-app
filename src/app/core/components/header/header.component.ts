068a2c604a62a2b1afb1d5c1a006d827839a97cc
Ihor Osadchyi <igorosa.com@gmail.com>
2021-12-12 15:00:45 +0200

    feat: header login/logout btn if authorized user

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
+  public isLoggedIn!: boolean;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIcon(
+      'logout',
+      sanitizer.bypassSecurityTrustResourceUrl('/assets/logout.svg')
+    );
+
+    this.authService.currentUser.subscribe((user) => {
+      this.isLoggedIn = !!user.access_token;
+      console.log(user);
+    });
+  }
 
   goToRoute(link: string = '/') {
     this.router.navigateByUrl(link);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
 }
