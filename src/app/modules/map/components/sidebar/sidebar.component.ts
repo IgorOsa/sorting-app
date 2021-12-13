441d2e76d4dd59189e1e4ebd7b9f754aec7a5d45
Ihor Osadchyi <igorosa.com@gmail.com>
2021-12-13 18:34:57 +0200

    fix: styles

--- src/app/modules/map/components/sidebar/sidebar.component.ts
+++ src/app/modules/map/components/sidebar/sidebar.component.ts
@@ -4,7 +4,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  // encapsulation: ViewEncapsulation.None,
 })
 export class SidebarComponent {
   wasteTypes: string[] = [
