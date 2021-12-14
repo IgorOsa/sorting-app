27be17b55c398398260cb7774bfdf76beab1eb8a
Ihor Osadchyi <igorosa.com@gmail.com>
2021-12-14 10:03:43 +0200

    fix: sidebar use data from service

--- src/app/modules/map/components/sidebar/sidebar.component.ts
+++ src/app/modules/map/components/sidebar/sidebar.component.ts
@@ -1,30 +1,52 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
-  wasteTypes: string[] = [
-    'Organic',
-    'Plastic',
-    'Paper/Cardboard',
-    'Hazardous',
-    'Metal',
-    'Glass',
-    'Electrical',
-    'Reusable things',
-    'Bulky/Construction',
-  ];
-  paymentTypes: string[] = ['Paid', 'Free'];
-  deliveryTypes: string[] = [
-    'None',
-    'From appartment door',
-    'From certain location',
-    'From street',
-  ];
+export class SidebarComponent implements OnInit {
+  wasteTypes!: string[];
+  paymentTypes!: string[];
+  deliveryTypes!: string[];
   selectedDeliveryType?: string;
+
+  public toolbarData!: any;
+  typesFormGroup!: FormGroup;
+  paymentFormGroup!: FormGroup;
+  deliveryFormGroup!: FormGroup;
+  deliveryChecked!: string;
+
+  constructor(private dataService: DataService, private fb: FormBuilder) {
+    this.toolbarData = this.dataService.store$.value.toolbarData;
+    this.wasteTypes = this.dataService.store$.value.toolbarData.types;
+    this.paymentTypes = this.dataService.store$.value.toolbarData.payment;
+    this.deliveryTypes = this.dataService.store$.value.toolbarData.delivery;
+  }
+
+  toFormGroup(questions: any[]) {
+    const group: any = {};
+
+    questions.forEach((question) => {
+      group[question] = question.required
+        ? new FormControl(question.value || false, Validators.required)
+        : new FormControl(question.value || false);
+    });
+    return new FormGroup(group);
+  }
+
+  ngOnInit(): void {
+    this.typesFormGroup = this.toFormGroup(this.toolbarData.types);
+    this.paymentFormGroup = this.toFormGroup(this.toolbarData.payment);
+    this.deliveryFormGroup = this.toFormGroup(this.toolbarData.delivery);
+  }
 
   wasteTypesPanelOpenState = false;
   paymentPanelOpenState = false;
