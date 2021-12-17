05d3392dfa5e7402de931dd21b99849523ad5d2c
Ihor Osadchyi <igorosa.com@gmail.com>
2021-12-17 09:03:40 +0200

    feat: add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './modules/user/components/login/login.component';
 import { RegisterComponent } from './modules/user/components/register/register.component';
 
@@ -21,6 +22,7 @@ const routes: Routes = [
     path: 'admin',
     loadChildren: () =>
       import('./modules/admin/admin.module').then((m) => m.AdminModule),
+    canLoad: [AuthGuard],
   },
   {
     path: 'user',
